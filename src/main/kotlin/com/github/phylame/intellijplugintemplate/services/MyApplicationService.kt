2d99f05ae461a95e11f2c27c1cef27264e2959de
GitHub Action <action@github.com>
2022-10-26 02:26:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/phylame/intellijplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/phylame/intellijplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.phylame.intellijplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phylame.intellijplugintemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/phylame/intellijplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/phylame/intellijplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.phylame.intellijplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phylame.intellijplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
